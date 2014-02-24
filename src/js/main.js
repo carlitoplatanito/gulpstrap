ba2647f994ecc1c428928abae31d8525db368ef9
Carlos Limardo <climardo@besentient.com>
2014-02-24 11:56:35 -0500

    Updated js file to select active page….

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,11 @@
 $(function() {
-    var currentUrl = String(document.location);
+    var currentUrl = new String(document.location);
+
+    // no file specified/index
+    if (currentUrl.indexOf('.html') <= -1) {
+        currentUrl += '/index.html';
+    }
+
     $('.nav.navbar-nav > li').each(function(){
         var link = $(this);
         if ( currentUrl.indexOf(link.find('a').attr('href')) > -1) {
